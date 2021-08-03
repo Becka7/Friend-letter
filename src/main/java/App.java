d8a8d0b73d4438c3d72a050ec16598bd367d15c5
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-04 02:18:41 +0300

    created templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,45 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
+
 public class App {
-     public static void main(String[] args) {
-         staticFileLocation("/public");
-        get("/",(request, response) ->
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
 
-        "<!DOCTYPE html>" +
-         "<html>" +
-                 "<head>" +
-                 "<title>Hello</title>" +
-                 "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                 "</head>" +
-                 "<body>" +
-                 "<h1>Hi there mate!....my name is Rabecca </h1>" +
-                 "<p>Dear Friend,</p>" +
-                 "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                 "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                 "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                 "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                 "<p>Cheers,</p>" +
-                 "<p> <a href ='/favourite-photos'>Checkout some of the photos i took</a> </p>"+
-                 "<p>Travel Enthusiast Jane</p>" +
-                 "</body>" +
-                 "</html>"
-        );
-        get("/favourite-photos",(request, response) ->
-        "<!DOCTYPE html>"+
-                "<html>" +
-                "<head>" +
-                "<title> Pictures </title>"+
-                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                "</head>"+
-                "<body>" +
-                "<h1>Photos from the trip</h1>"+
-                "<ul>" +
-                "<li><img src = '/images/javaimage.jpeg' alt='A photo of a mountain range.'/> </li>"+
-                "<li><img src = '/images/image2.jpeg'/> </li>"+
-                "</ul>"+
-                "</body>"+
-                "</html>"
+        }, new HandlebarsTemplateEngine());
 
-        );
 
-    };
+        get("/favourite-photos", (request, response) -> {
+            return new ModelAndView( new HashMap(), "favourite-photos.hbs");
+                }, new HandlebarsTemplateEngine());
+
+
+
+
+    }
 }
+
